f6645b6dc213bedd12cd179f44cb8a14dc764102
jbok24 <jamaalbokhari045@gmail.com>
2023-06-20 16:51:01 -0400

    WIP

--- terraform/backstage.tf
+++ terraform/backstage.tf
@@ -22,39 +22,6 @@ resource "kubernetes_secret" "postgres-secrets" {
 
 #POSTGRE Persistant Volume
 
-/*
-# kubernetes/postgres-storage.yaml
-apiVersion: v1
-kind: PersistentVolume
-metadata:
-  name: postgres-storage
-  namespace: backstage
-  labels:
-    type: local
-spec:
-  storageClassName: manual
-  capacity:
-    storage: 2G
-  accessModes:
-    - ReadWriteOnce
-  persistentVolumeReclaimPolicy: Retain
-  hostPath:
-    path: '/mnt/data'
----
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: postgres-storage-claim
-  namespace: backstage
-spec:
-  storageClassName: manual
-  accessModes:
-    - ReadWriteOnce
-  resources:
-    requests:
-      storage: 2G
-*/
-
 resource "kubernetes_persistent_volume" "postgres-persistent" {
   metadata {
     name      = "postgres-storage"
@@ -91,39 +58,6 @@ resource "kubernetes_persistent_volume_claim" "postgres-persistent" {
 
 
 # POSTGRE Deployment
-
-# kubernetes/postgres.yaml
-/*apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: postgres
-  namespace: backstage
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: postgres
-  template:
-    metadata:
-      labels:
-        app: postgres
-    spec:
-      containers:
-        - name: postgres
-          image: postgres:13.2-alpine
-          imagePullPolicy: 'IfNotPresent'
-          ports:
-            - containerPort: 5432
-          envFrom:
-            - secretRef:
-                name: postgres-secrets
-          volumeMounts:
-            - mountPath: /var/lib/postgresql/data
-              name: postgresdb
-      volumes:
-        - name: postgresdb
-          persistentVolumeClaim:
-            claimName: postgres-storage-claim */
     
 resource "kubernetes_deployment" "postgres-deployment" {
   metadata {
@@ -176,7 +110,6 @@ resource "kubernetes_deployment" "postgres-deployment" {
 }
 
 # POSTGRE Service
-
 
 resource "kubernetes_api_service" "postgres-service" {
   metadata {
